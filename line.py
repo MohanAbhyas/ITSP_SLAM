8e97876ce54fd79dd8f2774eb4cbe4e93fd2a319
Athul C D <34231792+athulcd@users.noreply.github.com>
2018-06-23 21:43:09 +0530

    Adding program to draw line

---
+++ line.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+
+list = [[110,12], [150, 60], [150, 80],[102,105], [120, 50] , [ 60,10] , [ 120, 50] , [150,80]]
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+for i in range(1,7):
+	img = cv2.line(img,(list[i][0],list[i][1]),(list[i+1][0],list[i+1][1]),(255,0,0),5)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+   
+
