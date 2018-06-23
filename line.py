2fac63b3d5b08d8b37aadb3509d67a9839d7dc42
athulcd <athulcd@gmail.com>
2018-06-23 22:06:03 +0530

    modified line.py

--- line.py
+++ line.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 
 
-list = [[110,12], [150, 60], [150, 80],[102,105], [120, 50] , [ 60,10] , [ 120, 50] , [150,80]]
+list = [[110,12], [150, 60], [150, 480],[102,105], [120, 450] , [ 60,310] , [ 120, 50] , [150,80]]
 
 # Create a black image
 img = np.zeros((512,512,3), np.uint8)
 
 for i in range(1,7):
-	img = cv2.line(img,(list[i][0],list[i][1]),(list[i+1][0],list[i+1][1]),(255,0,0),5)
+	img = cv2.line(img,(list[i][0],list[i][1]),(list[i+1][0],list[i+1][1]),(0,255,0),5)
 
 cv2.imshow('image',img)
 cv2.waitKey(0)
